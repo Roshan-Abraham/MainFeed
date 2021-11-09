f89fb2a4ed8fe27cc6fadf7f6c308eb59202182e
dweeb <dweeby.user1@gmail.com>
2021-11-09 16:14:24 +0530

    Initial commit

---
+++ DateMF/FirebaseIO.py
@@ -0,0 +1,309 @@
+import pandas as pd
+import firebase_admin
+from firebase_admin import credentials
+from firebase_admin import firestore
+cred = credentials.Certificate('key.json')
+try:    
+    firebase_admin.initialize_app(cred)
+except:
+    pass
+db = firestore.client()
+
+
+class FireBase:
+
+    user_data = {}
+
+    def __init__(self):
+
+        # self.db = firestore.client()
+        self.user_data["Drinking"] = 'NA'
+        self.user_data["Smoking"] = 'NA'
+        self.user_data["Gender"] = 'NA'
+        self.user_data["Name"] = 'NA'
+        self.user_data["Id"] = 'NA'
+        self.user_data["SexPreference"] = 'NA'
+        self.user_data["Age"] = 'NA'
+        self.user_data["PAgeMax"] = 'NA'
+        self.user_data["PAgeMin"] = 'NA'
+
+    def GetD(self):
+        # db = firestore.client()
+        self.users_ref = db.collection(u'root')
+        self.docs = self.users_ref.stream()
+        for doc in self.docs:
+            self.user_data["Drinking"] = u'{}'.format(doc.to_dict()['Drinking'])
+            self.user_data["Smoking"] = u'{}'.format(doc.to_dict()['Smoking'])
+            self.user_data["Gender"] = u'{}'.format(doc.to_dict()['Gender'])
+            self.user_data["Name"] = u'{}'.format(doc.to_dict()['Name'])
+            # self.user_data["Id"] = u'{}'.format(doc.id)
+        return(self.user_data)
+
+    def DateGet(self, id):
+        self.user_ref = db.collection(u'DateUsers').document(id)
+        self.doc = self.user_ref.get()
+
+        tagData = {}
+        tagData["city"] = u'{}'.format(self.doc.to_dict()['city'])
+        
+        # for doc in docs:
+        self.user_data["Drinking"] = u'{}'.format(self.doc.to_dict()['drinking'])
+        self.user_data["Smoking"] = u'{}'.format(self.doc.to_dict()['smoking'])
+        self.user_data["Gender"] = u'{}'.format(self.doc.to_dict()['gender'])
+        self.user_data["Age"] = u'{}'.format(self.doc.to_dict()['age'])
+        self.user_data["PrefDrinking"] = u'{}'.format(self.doc.to_dict()['pDrinking'])
+        self.user_data["PrefSmoking"] = u'{}'.format(self.doc.to_dict()['pSmoking'])
+        self.user_data["SexPreference"] = u'{}'.format(self.doc.to_dict()['wishToMeet'])
+        self.user_data["AgeRangeS"] = u'{}'.format(self.doc.to_dict()['preffAge']['start'])
+        self.user_data["AgeRangeE"] = u'{}'.format(self.doc.to_dict()['preffAge']['end'])
+
+        #count of existing documents in users BestMatches
+        # collectBM = db.collection('DateUsers').document(id).collection('BestMatches')
+        # collectionLen = len(list(collectBM.get()))
+        # #count of existing documents in users Swiped sub-collection
+        # collectSM = db.collection('DateUsers').document(id).collection('Swiped')
+        # collectionLen = collectionLen + len(list(collectSM.get()))
+
+        #list existing uuid in sub-collections bestmatches
+        CurrentQueryBM = db.collection('DateUsers').document(id).collection('BestMatches')
+        CurrentListBM = [docls.id for docls in CurrentQueryBM.stream()]
+
+        CurrentQuerySM = db.collection('DateUsers').document(id).collection('Swiped')
+        CurrentList = [docls.id for docls in CurrentQuerySM.stream()]
+        # CurrentListSM = []
+        # CurrentListBM = []
+        CurrentList.append(CurrentListBM)
+
+
+        # unfit = db.collection('DateUsers').where(u'Smoking', u'==', self.user_data["Smoking"]).where(u'Drinking', u'==', self.user_data["Drinking"])
+        if self.user_data["SexPreference"] == 'both':
+            # print('Entering first if case')
+            all_users = db.collection('DateUsers').where(u'age', u'>=', int(self.user_data['AgeRangeS'])).where(u'age', u'<=', int(self.user_data['AgeRangeE'])).where(u'smoking', u'==', self.user_data["PrefSmoking"]).where(u'drinking', u'==', self.user_data["PrefDrinking"]).limit(len(CurrentList) + 400)
+            all_user = [doc.to_dict() for doc in all_users.stream()]
+            all_id = [doc.id for doc in all_users.stream()]
+
+            df = pd.DataFrame.from_dict(all_user)
+            df['id'] = all_id
+
+            df = df[~df['id'].isin(CurrentList)]
+            df = df[0:400]
+
+            return df, tagData
+            # print('exit if case')
+        else:
+
+            all_users = db.collection('DateUsers').where(u'gender', u'==', self.user_data["SexPreference"]).where(u'age', u'>=', int(self.user_data['AgeRangeS'])).where(u'age', u'<=', int(self.user_data['AgeRangeE'])).where(u'smoking', u'==', self.user_data["PrefSmoking"]).where(u'drinking', u'==', self.user_data["PrefDrinking"]).limit(len(CurrentList) + 400)
+            all_user = [doc.to_dict() for doc in all_users.stream()]
+            all_id = [doc.id for doc in all_users.stream()]
+
+            df = pd.DataFrame.from_dict(all_user)
+            df['id'] = all_id
+
+            df = df[~df['id'].isin(CurrentList)]
+            df = df[0:400]
+
+            return df, tagData
+            # print('exit else case')
+
+    def DateGetLite(self, id):
+        self.user_ref = db.collection(u'DateUsers').document(id)
+        self.doc = self.user_ref.get()
+
+        tagData = {}
+        tagData["city"] = u'{}'.format(self.doc.to_dict()['city'])
+        
+        # for doc in docs:
+        # self.user_data["Drinking"] = u'{}'.format(self.doc.to_dict()['drinking'])
+        # self.user_data["Smoking"] = u'{}'.format(self.doc.to_dict()['smoking'])
+        # self.user_data["Gender"] = u'{}'.format(self.doc.to_dict()['gender'])
+        # self.user_data["Age"] = u'{}'.format(self.doc.to_dict()['age'])
+        # self.user_data["PrefDrinking"] = u'{}'.format(self.doc.to_dict()['pDrinking'])
+        # self.user_data["PrefSmoking"] = u'{}'.format(self.doc.to_dict()['pSmoking'])
+        self.user_data["SexPreference"] = u'{}'.format(self.doc.to_dict()['wishToMeet'])
+        self.user_data["AgeRangeS"] = u'{}'.format(self.doc.to_dict()['preffAge']['start'])
+        self.user_data["AgeRangeE"] = u'{}'.format(self.doc.to_dict()['preffAge']['end'])
+
+        #count of existing documents in users BestMatches
+        # collectBM = db.collection('DateUsers').document(id).collection('BestMatches')
+        # collectionLen = len(list(collectBM.get()))
+        # #count of existing documents in users Swiped sub-collection
+        # collectSM = db.collection('DateUsers').document(id).collection('Swiped')
+        # collectionLen = collectionLen + len(list(collectSM.get()))
+
+        #list existing uuid in sub-collections bestmatches
+        CurrentQueryBM = db.collection('DateUsers').document(id).collection('BestMatches')
+        CurrentListBM = [docls.id for docls in CurrentQueryBM.stream()]
+
+        CurrentQuerySM = db.collection('DateUsers').document(id).collection('Swiped')
+        CurrentList = [docls.id for docls in CurrentQuerySM.stream()]
+        # CurrentListSM = []
+        # CurrentListBM = []
+        CurrentList.append(CurrentListBM)
+
+
+        # unfit = db.collection('DateUsers').where(u'Smoking', u'==', self.user_data["Smoking"]).where(u'Drinking', u'==', self.user_data["Drinking"])
+        if self.user_data["SexPreference"] == 'both':
+            # print('Entering first if case')
+            all_users = db.collection('DateUsers').where(u'age', u'>=', int(self.user_data['AgeRangeS'])).where(u'age', u'<=', int(self.user_data['AgeRangeE'])).limit(len(CurrentList) + 400)
+            all_user = [doc.to_dict() for doc in all_users.stream()]
+            all_id = [doc.id for doc in all_users.stream()]
+
+            df = pd.DataFrame.from_dict(all_user)
+            df['id'] = all_id
+
+            df = df[~df['id'].isin(CurrentList)]
+            df = df[0:400]
+
+            return df, tagData
+            # print('exit if case')
+        else:
+
+            all_users = db.collection('DateUsers').where(u'gender', u'==', self.user_data["SexPreference"]).where(u'age', u'>=', int(self.user_data['AgeRangeS'])).where(u'age', u'<=', int(self.user_data['AgeRangeE'])).limit(len(CurrentList) + 400)
+            all_user = [doc.to_dict() for doc in all_users.stream()]
+            all_id = [doc.id for doc in all_users.stream()]
+
+            df = pd.DataFrame.from_dict(all_user)
+            df['id'] = all_id
+
+            df = df[~df['id'].isin(CurrentList)]
+            df = df[0:400]
+
+            return df, tagData
+    
+
+
+    def XGet(self, id):
+        self.user_ref = db.collection(u'DateUsers').document(id)
+        self.doc = self.user_ref.get()
+        # for doc in docs:
+        self.user_data["Drinking"] = u'{}'.format(self.doc.to_dict()['drinking'])
+        self.user_data["Smoking"] = u'{}'.format(self.doc.to_dict()['smoking'])
+        self.user_data["Gender"] = u'{}'.format(self.doc.to_dict()['gender'])
+        self.user_data["Age"] = u'{}'.format(self.doc.to_dict()['age'])
+        self.user_data["PrefDrinking"] = u'{}'.format(self.doc.to_dict()['pDrinking'])
+        self.user_data["PrefSmoking"] = u'{}'.format(self.doc.to_dict()['pSmoking'])
+        self.user_data["SexPreference"] = u'{}'.format(self.doc.to_dict()['wishToMeet'])
+        self.user_data["AgeRangeS"] = u'{}'.format(self.doc.to_dict()['preffAge']['start'])
+        self.user_data["AgeRangeE"] = u'{}'.format(self.doc.to_dict()['preffAge']['end'])
+
+        # unfit = db.collection('DateUsers').where(u'Smoking', u'==', self.user_data["Smoking"]).where(u'Drinking', u'==', self.user_data["Drinking"])
+        if self.user_data["SexPreference"] == 'both':
+            print('Entering first if case')
+            all_users = db.collection('DateUsers').where(u'age', u'>=', int(self.user_data['AgeRangeS'])).where(u'age', u'<=', int(self.user_data['AgeRangeE'])).where(u'Smoking', u'==', self.user_data["PrefSmoking"]).where(u'Drinking', u'==', self.user_data["PrefDrinking"]).limit(2)
+            print('exit if case')
+        else:
+
+            all_users = db.collection('DateUsers').where(u'gender', u'==', self.user_data["SexPreference"]).where(u'age', u'>=', int(self.user_data['AgeRangeS'])).where(u'age', u'<=', int(self.user_data['AgeRangeE'])).where(u'Smoking', u'==', self.user_data["PrefSmoking"]).where(u'Drinking', u'==', self.user_data["PrefDrinking"]).limit(2)
+            print('exit else case')
+
+        all_user = [doc.to_dict() for doc in all_users.stream()]
+        # all_id = [doc.id for doc in all_users.stream()]
+        # # all_id = []
+        # # for docx in all_users.stream():
+        # #     all_id = f'{docx.id} => {docx.to_dict()}' 
+
+        df = pd.DataFrame.from_dict(all_user)
+        # # dfid = pd.DataFrame.from_dict(all_id)
+        # df['id'] = all_id
+
+        return df
+        
+
+    def UGet(self, id):
+        self.user_ref = db.collection(u'DateUsers').document(id)
+        self.docs = self.user_ref.get()
+        # for doc in docs:
+        self.user_data["Drinking"] = u'{}'.format(self.docs.to_dict()['Drinking'])
+        self.user_data["Smoking"] = u'{}'.format(self.docs.to_dict()['Smoking'])
+        self.user_data["Gender"] = u'{}'.format(self.docs.to_dict()['gender'])
+        # self.user_data["Name"] = u'{}'.format(self.docs.to_dict()['n'])
+
+        self.user_data["SexPreference"] = u'{}'.format(self.docs.to_dict()['Wish to Meet'])
+        self.user_data["Age"] = u'{}'.format(self.docs.to_dict()['age'])
+        self.user_data["PAgeMax"] = u'{}'.format(self.docs.to_dict()['Preferred partner max age'])
+        self.user_data["PAgeMin"] = u'{}'.format(self.docs.to_dict()['Preferred partner min age'])
+
+        # unfit = db.collection('DateUsers').where(u'Smoking', u'==', self.user_data["Smoking"]).where(u'Drinking', u'==', self.user_data["Drinking"])
+        if self.user_data["SexPreference"] == 'both':
+            print('Entering first if case')
+            all_users = db.collection('DateUsers').where(u'Smoking', u'==', self.user_data["Smoking"]).where(u'Drinking', u'==', self.user_data["Drinking"])
+            print('exit if case')
+        else:
+
+            all_users = db.collection('DateUsers').where(u'Gender', u'==', self.user_data["SexPreference"]).where(u'Smoking', u'==', self.user_data["Smoking"]).where(u'Drinking', u'==', self.user_data["Drinking"])
+            print('exit else case')
+
+        all_user = [doc.to_dict() for doc in all_users.stream()]
+        all_id = [doc.id for doc in all_users.stream()]
+        # all_id = []
+        # for docx in all_users.stream():
+        #     all_id = f'{docx.id} => {docx.to_dict()}' 
+
+        df = pd.DataFrame.from_dict(all_user)
+        # dfid = pd.DataFrame.from_dict(all_id)
+        df['id'] = all_id
+
+        return df
+        
+    def SGet(self, id):
+        self.user_ref = db.collection(u'DateUsers').document(id)
+        self.docs = self.user_ref.get()
+        # for doc in docs:
+        self.user_data["Drinking"] = u'{}'.format(self.docs.to_dict()['Drinking'])
+        self.user_data["Smoking"] = u'{}'.format(self.docs.to_dict()['Smoking'])
+        self.user_data["Gender"] = u'{}'.format(self.docs.to_dict()['Gender'])
+        # self.user_data["Name"] = u'{}'.format(self.docs.to_dict()['n'])
+
+        self.user_data["SexPreference"] = u'{}'.format(self.docs.to_dict()['Wish to Meet'])
+        self.user_data["Age"] = u'{}'.format(self.docs.to_dict()['Age'])
+        self.user_data["PAgeMax"] = u'{}'.format(self.docs.to_dict()['Preferred partner max age'])
+        self.user_data["PAgeMin"] = u'{}'.format(self.docs.to_dict()['Preferred partner min age'])
+
+        unfit = db.collection('DateUsers').where(u'Smoking', u'==', self.user_data["Smoking"]).where(u'Drinking', u'==', self.user_data["Drinking"]).limit(5)
+
+        # x = f'{self.docs.to_dict()}'
+        # x = [doc.id for doc in unfit.stream()]
+        x = [doc.to_dict() for doc in unfit.stream()]
+        # x = pd.DataFrame.from_dict(x)
+        return x
+
+    def SendD(self, cluster, id):
+        self.users_refx = db.collection(u'root').document(
+            id).collection(u'OnBdCluster')
+        self.users_refx.set({u'ClusterNumber': cluster}, merge=True)
+        return 'ClusterID uploaded'
+
+def MCFunc():
+    x = FireBase()
+    see = x.GetD()
+    # print(see)
+    return see
+
+
+def EAFunc(persona, id):
+    x = FireBase()
+    x.SendD(persona, id)
+
+def DGet(id, smoke):
+    FireBase.GetD()
+    all_users = db.collection('DummyMLAIHardik').where(u'g', u'==', u'Male').where(u's', u'==', smoke)#.where(u'Drinking', u'==', drink)
+
+    all_users = [doc.to_dict() for doc in all_users.stream()]
+
+    df = pd.DataFrame.from_dict(all_users)
+
+def SendDocDate(x,id):
+    for i in x:
+        db.collection(u'DateUsers').document(id).collection(u'BestMatches').document(i['id']).set(i)
+    return 'MFList uploaded'
+
+def DyUpdate(x, id):
+    new_user = db.collection(u'DateUsers').document(id).get()
+    new_user_id = u'{}'.format(new_user.id)
+    new_user= f'{new_user.to_dict()}'
+    # print(type(x))
+    # print(new_user)
+    # for i in x:
+    #     print(i)
+    db.collection(u'DateUsers').document(x).collection(u'MainFeed').document(new_user_id).set(new_user)
+    
---
+++ DateMF/main.py
@@ -0,0 +1,145 @@
+# from FirebaseIO import SendD
+import os
+from flask import Flask, request, jsonify
+# from firebase_admin import credentials, firestore, initialize_app
+# from google.cloud import storage
+from ModelPred import PredCluster
+from FirebaseIO import FireBase, SendDocDate
+# import pandas as pd
+from datetime import date
+
+app = Flask(__name__)
+
+def dateDiff(startDate):
+    diff = date.today() - startDate.date()
+    if diff.days >= 31:
+        return True
+    else:
+        return False
+
+@app.route('/')
+def entry():
+    return 'DateUser BestMatches entry point API'
+
+
+@app.route('/test')
+def entrytest():
+    id = request.args.get('id')
+#Get list of filtered users/ in DataFrame format
+    y = FireBase()
+    df, temper = y.DateGet(id)
+    temper = 0
+    df = df.to_json(orient ='records')
+    df = df.replace("true", "True")
+    df = df.replace("false", "False")
+    df = eval(df)
+    return str(df) #str(type(df)), 200
+#function for OrderScore based on plan
+def order(plan, score):
+    return ((score) - 4) if plan == 'celeb' else( ((score) - 6) if plan == 'elite' or 'limelight' else ((score) - 10))
+
+
+@app.route('/datemf', methods=['GET','POST'])
+def MFlist():
+    # try:
+
+#Identify Anchor (Main) User 
+    id = request.args.get('id')
+#Get list of filtered users/ in DataFrame format
+    y = FireBase()
+    df, tagData = y.DateGet(id)
+
+    
+    dfcp = df[['personalityTraits', 'interests']]
+    dfcp.rename(columns = {'personalityTraits':'Personality Traits', 'interests':'Interests'}, inplace = True)
+
+    # df = pd.DataFrame.from_dict(all_users)
+    # df = df.drop('Name')
+    # df.set_index("Name", inplace=True)
+    out = PredCluster(dfcp)
+    outdf = df.loc[out.index]
+    
+    #Convert value to percentage scale and limiting with -10 to control plan improvement
+    outdf['ComScore'] = (out*100) - 10
+
+    outdf['OrderScore'] = outdf[['plan','ComScore']].apply(lambda x: order(*x), axis=1)
+
+    #function for nerby location
+    #logic currently based on same city of users instead of location radius 
+    outdf['nearby'] = outdf['city'].apply(lambda x: True if x == tagData['city'] else False)
+
+    outdf['new'] = outdf['createdAt'].apply(lambda x: dateDiff(x))
+
+
+    outdf = outdf.to_json(orient ='records')
+    outdf = outdf.replace("true", "True")
+    outdf = outdf.replace("false", "False")
+    # bestMatchDF = outdf[['uuid','name','age','designation','isVerified','dpUrl','imgUrls','prompts','OrderScore','ComScore','interests','personalityTraits','new','nearby']]
+    bestMatchDF = eval(outdf)
+    SendDocDate(bestMatchDF, id)    # SendDes(outdf, id)
+    
+    return 'Docs uploaded', 200
+    # except Exception as e:
+    #     return f"An Error Occured: {e}"  
+
+
+@app.route('/datemflite', methods=['GET','POST'])
+def MFlistlite():
+    # try:
+
+#Identify Anchor (Main) User 
+    id = request.args.get('id')
+    if not id:
+        msg = "no id received"
+        print(f"error: {msg}")
+        return f"Bad Request: {msg}", 400       
+#Get list of filtered users/ in DataFrame format
+    y = FireBase()
+    df, tagData = y.DateGetLite(id)
+
+    
+    dfcp = df[['personalityTraits', 'interests']]
+    dfcp.rename(columns = {'personalityTraits':'Personality Traits', 'interests':'Interests'}, inplace = True)
+
+    # Sending Only 2 columns 'Personality Traits' & 'Interests' to DateMainFeed model in ModelPred scipt for preprocessing and prediction
+    out = PredCluster(dfcp)
+
+    #Adding output score from model(array) to existing Dataframe to reduce procing in mocving large amounts of data
+    #index is static and unchanging between reading and  model prediction
+    outdf = df.loc[out.index]
+    
+    #Convert value to percentage scale and limiting with -10 to control plan improvement
+    outdf['ComScore'] = (out*100) - 10
+
+    #Add field for ordering list of users to view based on the users purchased plans
+    outdf['OrderScore'] = outdf[['plan','ComScore']].apply(lambda x: order(*x), axis=1)
+
+    #function for nerby location
+    #logic currently based on same city of users instead of location radius 
+    #value in boolean format
+    outdf['nearby'] = outdf['city'].apply(lambda x: True if x == tagData['city'] else False)
+
+    #Unsure how logic will hold up  for users in bottom of list who will be viewed late
+    #compare date of creation from current date
+    #value in boolean format
+    outdf['new'] = outdf['createdAt'].apply(lambda x: dateDiff(x))
+
+
+    outdf = outdf.to_json(orient ='records')
+    #converting boolean values into lower caps for firebase purposes 
+    outdf = outdf.replace("true", "True")
+    outdf = outdf.replace("false", "False")
+    # bestMatchDF = outdf[['uuid','name','age','designation','isVerified','dpUrl','imgUrls','prompts','OrderScore','ComScore','interests','personalityTraits','new','nearby']]
+    
+    #formating dict for uploading into firestoer DB
+    bestMatchDF = eval(outdf)
+
+    #Uploading function
+    SendDocDate(bestMatchDF, id)
+    NoDocs = len(bestMatchDF)
+    return f'Docs uploaded {NoDocs}', 200
+
+
+port = int(os.environ.get('PORT', 8080))
+if __name__ == '__main__':
+    app.run(debug=True, threaded=True, host='0.0.0.0', port=port)
